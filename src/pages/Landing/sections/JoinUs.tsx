736fddbde3313a984922f0281f67741ddaeda04b
Ibrahim Mahrous <ebraheemalimahrous000@gmail.com>
2025-03-02 00:34:29 +0200

    Finished JoinUs Styles

--- src/pages/Landing/sections/JoinUs.tsx
+++ src/pages/Landing/sections/JoinUs.tsx
@@ -1,5 +1,9 @@
-
-
+// ** Styles
+import style from '../../../styles/pages/Landing/JoinUs.module.css'
+// ** Assets
+import joinUsPhoto from '../../../assets/landing/JoinUs/join-us.png'
+// ** Hooks
+import { useNavigate } from 'react-router-dom'
 
 
 
@@ -9,11 +13,22 @@ interface ISection{
 }
 
 export default function JoinUs({sectionId}: ISection) {
+    const navigate = useNavigate();
+    const signUpHandler = () => {navigate('/u/sign-up')}
     return (
         <>
-            <section id = {sectionId}>
-                <div style={{textAlign: 'center' , margin: '280px'}}>
-                    JoinUs
+            <section className= {style.section} id = {sectionId}>
+                <div className= {style.section_container}>
+                    <div className= {style.section_content}>
+                        <div className= {style.join_Us_Photo}>
+                            <img src= {joinUsPhoto} alt="upload" />
+                        </div>
+                        <div className= {style.join_Us_Text}>
+                            <h3>رفع الصورة لتحليل فوري ودعم مباشر:</h3>
+                            <p>يمكنك رفع صورة الأشعة الطبية الخاصة بك والحصول على تقرير سريع حول حالتك الصحية. يساعدك هذا التحليل في فهم حالة الكلى بشكل أفضل وتحديد الخطوات المناسبة. إذا كنت ترغب في استشارات إضافية ودعم مستمر، يمكنك إنشاء حساب للحصول هذه الخدمات.</p>
+                            <button onClick={signUpHandler}>إنشاء حساب</button>
+                        </div>
+                    </div>
                 </div>  
             </section>  
         </>
